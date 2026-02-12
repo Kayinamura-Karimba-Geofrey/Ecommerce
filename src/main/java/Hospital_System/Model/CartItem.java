7c238ffb97682e9907f8fbc63640be13d4c8204b
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-12 14:38:45 +0200

    e-commerce

--- src/main/java/Hospital_System/Model/CartItem.java
+++ src/main/java/Hospital_System/Model/CartItem.java
@@ -1,4 +1,48 @@
 package Hospital_System.Model;
 
 public class CartItem {
+    private int id ;
+    private int UserId;
+    private int productId;
+    private int quantity;
+    public CartItem() {
+    }
+    public CartItem(int id,int UserId, int productId, int quantity){
+        this.id= id;
+        this.UserId= UserId;
+        this.productId= productId;
+        this.quantity= quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
