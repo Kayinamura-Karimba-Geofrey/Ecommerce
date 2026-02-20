4539fd6aaae7c4a759189044aeb4c25c47e0f511
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-20 21:48:05 +0200

    jwt errors solved

--- src/main/java/ecommerce/Services/UserService.java
+++ src/main/java/ecommerce/Services/UserService.java
@@ -1,4 +1,57 @@
 package ecommerce.Services;
 
+import ecommerce.Model.User;
+import ecommerce.Util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 public class UserService {
+
+    public void saveUser(User user) {
+        Transaction transaction = null;
+
+        try (Session session =
+                     HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public User login(String email, String password) {
+
+        try (Session session =
+                     HibernateUtil.getSessionFactory().openSession()) {
+
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE email = :email AND password = :password",
+                    User.class);
+
+            query.setParameter("email", email);
+            query.setParameter("password", password);
+
+            return query.uniqueResult();
+        }
+    }
+
+    public boolean emailExists(String email) {
+
+        try (Session session =
+                     HibernateUtil.getSessionFactory().openSession()) {
+
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE email = :email",
+                    User.class);
+
+            query.setParameter("email", email);
+
+            return query.uniqueResult() != null;
+        }
+    }
 }
