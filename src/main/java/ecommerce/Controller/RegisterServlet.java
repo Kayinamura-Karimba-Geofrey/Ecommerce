b55fda65437398e52fb14f18a5a1908f706b6795
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-20 22:08:14 +0200

    jwt errors solved

--- src/main/java/ecommerce/Controller/RegisterServlet.java
+++ src/main/java/ecommerce/Controller/RegisterServlet.java
@@ -1,21 +1,45 @@
 package ecommerce.Controller;
-import jakarta.servlet.ServletException;
+import ecommerce.Services.UserService;
+import ecommerce.Model.User;
+
+import ecommerce.Services.UserService;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
 
 import java.io.IOException;
-
-@WebServlet(name = "RegisterServlet", value = "/register")
+import java.util.List;
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    private UserService userDAO;
+
+    @Override
+    public void init() {
+        userDAO = new UserService();
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
 
+        String name = request.getParameter("fullname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+        if (userDAO.emailExists(email)) {
+            request.setAttribute("error",
+                    "Email already registered!");
+            request.getRequestDispatcher("register.jsp")
+                    .forward(request, response);
+            return;
+        }
+
+        User user = new User(fullname, email, password,role);
+        userDAO.saveUser(user);
+
+        response.sendRedirect("login.jsp");
     }
 }
