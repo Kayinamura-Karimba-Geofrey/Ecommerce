faae830c5d4dcd1dd7dfcf8124cf1fb8eb8c20f9
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-20 22:11:46 +0200

    jwt errors solved

--- src/main/java/ecommerce/Controller/LoginServlet.java
+++ src/main/java/ecommerce/Controller/LoginServlet.java
@@ -1,21 +1,46 @@
 package ecommerce.Controller;
+
+import ecommerce.Services.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", value = "/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    private UserService userDAO;
+
+    @Override
+    public void init() {
+        userDAO = new UserService();
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
 
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserService user = user.login(email, password);
+
+        if (user == null) {
+            request.setAttribute("error",
+                    "Invalid email or password!");
+            request.getRequestDispatcher("login.jsp")
+                    .forward(request, response);
+        } else {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("loggedUser", user);
+
+            response.sendRedirect("products");
+        }
     }
 }
