9e51b29202bf71f8117b9f02b74ce22a6003032b
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-21 20:14:58 +0200

    jwt errors solved

--- src/main/java/ecommerce/filter/AuthFilter.java
+++ src/main/java/ecommerce/filter/AuthFilter.java
@@ -1,40 +1,40 @@
-//package ecommerce.filter;
-//
-//import jakarta.servlet.*;
-//import jakarta.servlet.annotation.WebFilter;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//
-//import java.io.IOException;
-//
-///*
-//@WebFilter(urlPatterns = {
-//        "/products",
-//        "/cart",
-//        "/checkout"
-//})
-//*/
-//public class AuthFilter implements Filter {
-//
-//    @Override
-//    public void doFilter(ServletRequest request,
-//                         ServletResponse response,
-//                         FilterChain chain)
-//            throws IOException, ServletException {
-//
-//        HttpServletRequest req = (HttpServletRequest) request;
-//        HttpServletResponse res = (HttpServletResponse) response;
-//
-//        HttpSession session = req.getSession(false);
-//
-//        boolean loggedIn = (session != null &&
-//                session.getAttribute("loggedUser") != null);
-//
-//        if (loggedIn) {
-//            chain.doFilter(request, response);
-//        } else {
-//            res.sendRedirect(req.getContextPath() + "/login.jsp");
-//        }
-//    }
-//}
+package ecommerce.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/*
+@WebFilter(urlPatterns = {
+        "/products",
+        "/cart",
+        "/checkout"
+})
+*/
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+
+        boolean loggedIn = (session != null &&
+                session.getAttribute("loggedUser") != null);
+
+        if (loggedIn) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(req.getContextPath() + "/login.jsp");
+        }
+    }
+}
