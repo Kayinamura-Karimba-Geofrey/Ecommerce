a60ec5f7fc1d927c2c10e0e5c7fada2dc7a500ec
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-25 20:14:35 +0200

    products

--- src/main/java/ecommerce/Model/OrderItem.java
+++ src/main/java/ecommerce/Model/OrderItem.java
@@ -1,4 +1,30 @@
 package ecommerce.Model;
 
+package com.yourpackage.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int quantity;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItem() {}
+
+    // getters and setters
 }
