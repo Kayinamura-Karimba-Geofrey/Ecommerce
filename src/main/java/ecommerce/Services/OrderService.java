872fac6271719a3555abc27e21aaefba5ad5c5ee
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-02-25 20:12:50 +0200

    products

--- src/main/java/ecommerce/Services/OrderService.java
+++ src/main/java/ecommerce/Services/OrderService.java
@@ -1,4 +1,29 @@
 package ecommerce.Services;
 
-public class OrderService {
+
+import ecommerce.Model.Order;
+import com.yourpackage.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class OrderDAO {
+
+    public void saveOrder(Order order) {
+
+        Transaction transaction = null;
+
+        try (Session session =
+                     HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            session.save(order);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
 }
