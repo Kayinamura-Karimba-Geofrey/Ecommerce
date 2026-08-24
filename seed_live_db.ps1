9fef2904fd3243dbf2f49b8b673b6aa451f55284
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-08-24 22:03:21 +0200

    security: remove plain-text database credentials script

--- seed_live_db.ps1
+++
@@ -1,7 +0,0 @@
-# Temporary PowerShell script to seed the database
-$env:DB_URL="jdbc:postgresql://ep-polished-truth-aixpryt1-pooler.c-4.us-east-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_pSvgJZ6AqRm4&sslmode=require&channelBinding=require
-             $env:DB_USERNAME="neondb_owner""
-$env:DB_USERNAME="neondb_owner"
-$env:DB_PASSWORD="npg_pSvgJZ6AqRm4"
-
-mvn exec:java -Dexec.mainClass="ecommerce.Util.DatabaseSeeder"
